466a9efd1eb5fa6afaf024e1153aa980a9556d32
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-05-03 14:01:01 -0400

    Add user profile, user update and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,11 +21,15 @@ import { UserRegistrationFormComponent } from './user-registration-form/user-reg
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UserProfileUpdateComponent } from './user-profile-update/user-profile-update.component';
 
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+  { path: 'profile', component: UserProfileComponent },
 ];
 
 @NgModule({
@@ -35,6 +39,9 @@ const appRoutes: Routes = [
     UserLoginFormComponent,
     MovieCardComponent,
     WelcomePageComponent,
+    UserProfileComponent,
+    NavBarComponent,
+    UserProfileUpdateComponent,
   ],
   imports: [
     BrowserModule,
