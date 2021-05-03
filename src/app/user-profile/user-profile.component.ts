ec18f9b483b5124687801a183a9ec28632aa7c6a
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-05-03 14:00:14 -0400

    Add user profile view

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+
+import { UserProfileUpdateComponent } from '../user-profile-update/user-profile-update.component';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css'],
+})
+export class UserProfileComponent implements OnInit {
+  user: any = {};
+  movies: any[] = [];
+  favorites: any = [];
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public router: Router,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialog
+  ) {}
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(): void {
+    this.fetchApiData.getUser().subscribe((res: any) => {
+      this.user = res;
+      this.getMovies();
+    });
+  }
+
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((res: any) => {
+      this.movies = res;
+      this.filterFavorites();
+    });
+  }
+
+  filterFavorites(): void {
+    this.movies.forEach((movie: any) => {
+      if (this.user.FavoriteMovies.includes(movie._id)) {
+        this.favorites.push(movie);
+      }
+    });
+    return this.favorites;
+  }
+
+  removeFavorites(id: string, title: string): void {
+    this.fetchApiData.deleteFavoriteMovie(id).subscribe(() => {
+      this.snackBar.open(
+        `${title} has been removed from your favourites!`,
+        'OK',
+        {
+          duration: 2000,
+        }
+      );
+      setTimeout(function () {
+        window.location.reload();
+      }, 1000);
+    });
+  }
+
+  deleteUser(): void {
+    let check = confirm(
+      'This will delete your profile! Are you sure you want to continue?'
+    );
+    if (check) {
+      this.fetchApiData.deleteUser().subscribe(() => {
+        localStorage.clear();
+        this.router.navigate(['welcome']);
+        this.snackBar.open('Profile deleted', 'OK', {
+          duration: 2000,
+        });
+      });
+    } else {
+      window.location.reload();
+    }
+  }
+
+  profileUpdateDialog(): void {
+    this.dialog.open(UserProfileUpdateComponent, {
+      width: '350px',
+    });
+  }
+}
